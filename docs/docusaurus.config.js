8bc99df0d89b55cd6665c4f7a12d5371203bab02
neugjq <3020829249@qq.com>
2026-02-25 23:56:26 +0800

    Configure Docusaurus for GitHub Pages deployment

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -29,6 +29,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'neugjq', // Usually your GitHub org/user name.
   projectName: 'homework', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
